3c59fc7bfb9ff4252d066eeb4f4bec1109fe7123
Andss-ye <andrex120tv@gmail.com>
2024-10-30 08:06:44 -0500

    feat: :lock: Anadido validacion de codigo vacio
    Valida que el usuario al registrar y al buscar no pueda ingresar un dato vacio

--- modules/ciudades.py
+++ modules/ciudades.py
@@ -3,7 +3,11 @@ from modules.validaciones import validacionCodigoRepetido
 def registrarCiudad(ciudades):
     codigo = validacionCodigoRepetido(str(input('\nIngrese el codigo de la ciudad a registrar: ')).upper(), ciudades)
     ciudad = str(input('\nIngrese el nombre de la ciudad: ')).upper()
-    
+
+    if codigo == '':
+        print('\nEl codigo a registrar no tiene que ser vacio')
+        return ciudades
+
     if codigo != -1:
         ciudades[codigo] = ciudad
         print(f'\nCiudad registrada con codigo {codigo} y nombre {ciudad}'.format(codigo=codigo, ciudad=ciudad))
@@ -14,6 +18,11 @@ def registrarCiudad(ciudades):
 
 def buscarCiudad(ciudades):
     codigo = str(input('\nIngrese el codigo de la ciudad a buscar: ')).upper()
+
+    if codigo == '':
+        print('\nEl codigo a buscar no puede ser vacio')
+        return 
+
     ciudad = ciudades.get(codigo)
 
     if ciudad:
