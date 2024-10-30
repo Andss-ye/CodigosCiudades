ed50b5735b42155e7cc77be0ed768bda5453ade9
Andss-ye <andrex120tv@gmail.com>
2024-10-30 08:09:59 -0500

    fix: :ambulance: Arreglado error en el menu
    Se arreglo error en el menu de ValueError, para que no ingrese datos invalidos

--- main.py
+++ main.py
@@ -15,22 +15,26 @@ ciudades = loadCities()
 opc = 10
 
 while opc != 0:
-    opc = menu()
-    if opc == 1:
-        print('Ciudad por código')
-        ciudades = registrarCiudad(ciudades)
-        saveCities(ciudades)
+    try:
+        opc = menu()
+        if opc == 1:
+            print('Ciudad por código')
+            ciudades = registrarCiudad(ciudades)
+            saveCities(ciudades)
 
-    elif opc == 2:
-        print('Registrar ciudad')
-        buscarCiudad(ciudades)
+        elif opc == 2:
+            print('Registrar ciudad')
+            buscarCiudad(ciudades)
 
-    elif opc == 3:
-        print('Ver todas las ciudades con sus códigos')
-        verCiudades(ciudades)
+        elif opc == 3:
+            print('Ver todas las ciudades con sus códigos')
+            verCiudades(ciudades)
 
-    elif opc == 0:
-        print('Salir')
-        exit()
-    else:
-        print('Opción incorrecta, revise y vuelva a intentar')
+        elif opc == 0:
+            print('Saliendo')
+            exit()
+        else:
+            print('Opción incorrecta, revise y vuelva a intentar')
+    
+    except ValueError:
+        print('Error, opción no válida, solo se permiten numeros enteros como opcion')
